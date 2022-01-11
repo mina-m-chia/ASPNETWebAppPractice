5cd405a4bf9fe939a4296d7438eade58cdebbc42
mina-m-chia <90296893+mina-m-chia@users.noreply.github.com>
2022-01-10 18:05:02 -0700

    initialization

---
+++ MyFirstASPNETWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,11 @@
+﻿//document.getElementById("btnSend").addEventListener("click", function () {
+//    alert("From: " + document.getElementById("txtFrom").value +
+//        ", Subject: " + document.getElementById("txtSubject").value +
+//        ", Message: " + document.getElementById("txtMessage").value);
+//})
+
+$("#btnSend").click(function () {
+    alert("From: " + $("#txtFrom").val() +
+        ", Subject: " + $("#txtSubject").val() +
+        ", Message: " + $("#txtMessage").val())
+})
